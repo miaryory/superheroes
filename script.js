ceb6e9b16c3106f0854c450cac80f7220048d94a
miaryory <miar0004@stud.kea.dk>
2019-09-23 11:44:36 +0200

    post function added

--- script.js
+++ script.js
@@ -12,17 +12,47 @@ function get() {
         .then(e => e.json())
         .then(superheroes => {
             superheroes.forEach(hero => {
-                const template = document.querySelector("template").content;
-                const clone = template.cloneNode(true);
-
-                clone.querySelector("h1").textContent = hero.name;
-                clone.querySelector("h2").textContent = hero.realname;
-                clone.querySelector(".age").textContent = hero.age + "yo";
-                clone.querySelector(".powers").textContent = hero.powers;
-
-                document.querySelector(".list").appendChild(clone);
+                displayHero(hero);
             });
         });
 }
 
 get();
+
+function displayHero(hero) {
+    const template = document.querySelector("template").content;
+    const clone = template.cloneNode(true);
+
+    clone.querySelector("h1").textContent = hero.name;
+    clone.querySelector("h2").textContent = hero.realname;
+    clone.querySelector(".age").textContent = hero.age + "yo";
+    clone.querySelector(".powers").textContent = hero.powers;
+
+    document.querySelector(".list").appendChild(clone);
+}
+
+function post() {
+    const data = {
+        name: "The Frontender2",
+        realname: "Miary",
+        powers: "JS",
+        age: 40
+    };
+
+    const postData = JSON.stringify(data);
+    fetch("https://kea3rdsemester-91fd.restdb.io/rest/superheroes", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "x-apikey": "5d887df9fd86cb75861e2626",
+                "cache-control": "no-cache"
+            },
+            body: postData
+        })
+        .then(res => res.json())
+        .then(data => {
+            displayHero(data);
+        });
+}
+
+document.querySelector("button").addEventListener("click", post);
